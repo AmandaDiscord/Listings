20107bf291488d94cf788ebe2ca546f2b62fd954
Cadence Ember <cloudrac3r@vivaldi.net>
2020-07-31 22:31:53 +1200

    Build script should log to stderr

--- build.fish
+++ build.fish
@@ -12,20 +12,20 @@ for site in (dirname (find -name meta.fish))
 		source meta.fish
 		set -l IFS
 		set -l output_file "../_build/$meta_name.md"
-		echo -n "Building $meta_name... "
+		echo -n "Building $meta_name... " >&2
 		cp output.template $output_file
 
 		cat $output_file | string replace @url $meta_url > $output_file; or exit
 		cat $output_file | string replace @markdown (cat ../common/template.md) > $output_file; or exit
 		test -e style.sass; and cat $output_file | string replace @sass (sass style.sass) > $output_file; or exit
 
-		echo 'done.'
+		echo 'done.' >&2
 	end
 
 	popd
 end
 
-echo 'All done, cleaning up temporary files.'
-rm -f build/*
+echo 'All done, cleaning up temporary files.' >&2
+test (count build/*) -gt 0; and rm -f build/*
 mv _build/* -t build
 rm -d _build
